77f9e8f5c1ac77c1f7eb30167bb58b1d7590d413
joeyscags <joeyscags5000@gmail.com>
2026-02-25 17:09:00 +0100

    implement shortestSpan and longestSpan methods with error handling for insufficient numbers

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -31,10 +31,28 @@ void Span::addNumber(int n)
 
 int Span::shortestSpan() const
 {
-	return 0;
+	if (_numbers.size() < 2)
+		throw std::underflow_error("Not enough numbers to find a span");
+
+	std::vector<int> sorted(_numbers);
+	std::sort(sorted.begin(), sorted.end());
+
+	int shortest = sorted[1] - sorted[0];
+	for (size_t i = 2; i < sorted.size(); i++)
+	{
+		int diff = sorted[i] - sorted[i - 1];
+		if (diff < shortest)
+			shortest = diff;
+	}
+	return shortest;
 }
 
 int Span::longestSpan() const
 {
-	return 0;
+	if (_numbers.size() < 2)
+		throw std::underflow_error("Not enough numbers to find a span");
+
+	int min = *std::min_element(_numbers.begin(), _numbers.end());
+	int max = *std::max_element(_numbers.begin(), _numbers.end());
+	return max - min;
 }
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
 
 class Span
 {
